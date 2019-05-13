ec217e033b5e7f45978e42214deb4011788a4253
snau57 <snau57@gmail.com>
2019-05-13 15:46:16 +0800

    Homework3

--- app/src/main/java/com/rachen0101/currency/MainActivity.java
+++ app/src/main/java/com/rachen0101/currency/MainActivity.java
@@ -11,60 +11,62 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText ed_ntd;
-    private Button bn_go;
-    private TextView tv_ntd;
-    private TextView tv_jpy;
-    private TextView tv_usd;
+    private EditText edNtd;
+    private Button bnGo;
+    private TextView tvNtd;
+    private TextView tvJpy;
+    private TextView tvUsd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ed_ntd = findViewById(R.id.ntd_in);
-        bn_go = findViewById(R.id.go);
-        tv_ntd = findViewById(R.id.ntd);
-        tv_jpy = findViewById(R.id.jpy);
-        tv_usd = findViewById(R.id.usd);
+        edNtd = findViewById(R.id.ntd_in);
+        bnGo = findViewById(R.id.go);
+        tvNtd = findViewById(R.id.ntd);
+        tvJpy = findViewById(R.id.jpy);
+        tvUsd = findViewById(R.id.usd);
         Button rate = findViewById(R.id.rate);
         rate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 new AlertDialog.Builder(MainActivity.this)
-                .setTitle("Exchange Rates")
-                .setMessage("NTD : JPY = 1 : 0.28\nNTD : USD = 1 : 30.9")
-                .setPositiveButton("OK", null)
+                .setTitle(R.string.exchange_rates)
+                .setMessage(getString(R.string.ntd) + " : " + getString(R.string.jpy) + " = 1 : 0.28\n" +
+                        getString(R.string.ntd) + " : " + getString(R.string.usd) + " = 1 : 30.9")
+                .setPositiveButton(R.string.ok, null)
                 .show();
             }
         });
     }
 
     public void clickGo(View view) {
-        String  n = ed_ntd.getText().toString();
+        String  n = edNtd.getText().toString();
         if (n.isEmpty()) {
             new AlertDialog.Builder(this)
-            .setTitle("Problem")
-            .setMessage("Please enter your NTD amount")
-            .setPositiveButton("OK", null)
+            .setTitle(R.string.problem)
+            .setMessage(R.string.your_ntd_amount)
+            .setPositiveButton(R.string.ok, null)
             .show();
         } else {
             float ntd = Float.parseFloat(n);
-            float result_jpy = (float)(ntd / 0.28);
-            float result_usd = (float)(ntd / 30.9);
+            float resultJpy = (float)(ntd / 0.28);
+            float resultUsd = (float)(ntd / 30.9);
             String nt = Float.toString(ntd);
-            String jp = Float.toString(result_jpy);
-            String us = Float.toString(result_usd);
-            tv_ntd.setText(nt + " = ");
-            tv_jpy.setText(jp);
-            tv_usd.setText(us);
+            String jp = Float.toString(resultJpy);
+            String us = Float.toString(resultUsd);
+            tvNtd.setText(nt + " = ");
+            tvJpy.setText(jp);
+            tvUsd.setText(us);
             new AlertDialog.Builder(this)
-            .setTitle("Result")
-            .setMessage("JPY is " + result_jpy + "\nUSD is " + result_usd)
-            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            .setTitle(R.string.result)
+            .setMessage(getString(R.string.jpy_is) + " " + resultJpy + "\n" +
+                    getString(R.string.usd_is) + " " + resultUsd)
+            .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
-                    ed_ntd.setText("");
+                    edNtd.setText("");
                 }
             })
             .show();
